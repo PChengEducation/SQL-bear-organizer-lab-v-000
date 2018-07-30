74dc9b98907265726dd6e82b3e8c02b3156d81ae
Pan Cheng <pchengeducation@hotmail.com>
2018-07-30 01:21:33 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (Mr.Chocolate, 1, M, black, nice, Y)
+INSERT INTO bears (Mr Chocolate, 1, M, black, nice, Y)
 INSERT INTO bears (Rowdy, 1, M, black, nice, Y)
 INSERT INTO bears (Tabitha, 1, M, black, nice, Y)
 INSERT INTO bears (Segeant Brown, 1, M, black, nice, Y)
